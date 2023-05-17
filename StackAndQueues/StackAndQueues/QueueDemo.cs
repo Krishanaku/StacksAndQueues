d93644e576d0ee084441bc6b77d530b44a0a7f0d
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-05-17 14:56:26 +0530

    Add : Queue Problem using method insert delete etc..

---
+++ StackAndQueues/StackAndQueues/QueueDemo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueues
+{
+    public class QueueDemo
+    {
+        public void QueueMethod()
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(10);
+            queue.Enqueue(21);
+            queue.Enqueue(61);
+            queue.Enqueue(132);
+            var remove = queue.Dequeue(); // it wil remove first element from queue
+            Console.WriteLine(remove);
+            foreach (var item in queue)
+            {
+                Console.WriteLine("Add :" + item);
+            }
+            //Console.WriteLine("Peek........");
+            //queue.Dequeue();
+            //foreach (var items in queue)
+            //{
+            //    Console.WriteLine("Remove :" + items);
+            //}
+            Console.WriteLine("Peek :" + queue.Peek());
+            // value is presenet or not.................
+            Console.WriteLine("Conatins :" + queue.Contains(31));
+
+            //Generic help to......
+
+
+        }
+        public void GenericQueMethod()
+        {
+            Queue<int> queueGen = new Queue<int>();
+            queueGen.Enqueue(189);
+            queueGen.Enqueue(27);
+            queueGen.Enqueue(31);
+            queueGen.Enqueue(46);
+            var deq = queueGen.Dequeue();
+            Console.WriteLine("Remove:" + deq);
+
+            foreach (var Gitem in queueGen)
+            {
+                Console.WriteLine(Gitem);
+            }
+
+        }
+    }
+}
